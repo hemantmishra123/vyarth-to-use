025cbc0dd24a0f52cfb6333afa21f62c669dfd31
Hemant Mishra <hemantmishra575@gmail.com>
2022-04-27 17:42:56 +0530

    added staticdirs

--- vyarth/settings.py
+++ vyarth/settings.py
@@ -131,14 +131,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 #django_heroku.settings(locals())
 #crispy forms settings
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 #Email settings
-
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
